6006757e07490330f6db2663c4c55df8e3b9fda8
dhemeira <71771187+ForexCaptain@users.noreply.github.com>
2022-10-28 19:22:48 +0200

    fix: vercel audiences missing import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import MainLayout from '../components/layout/mainLayout';
 import { MyThemeContextProvider } from '../store/myThemeContext';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
