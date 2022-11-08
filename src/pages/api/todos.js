db515cfafd9ecdc710680de9d524abbd4c0b98b1
dhemeira <71771187+ForexCaptain@users.noreply.github.com>
2022-11-08 01:01:25 +0100

    fix: add missing returns to api responses

--- src/pages/api/todos.js
+++ src/pages/api/todos.js
@@ -11,9 +11,9 @@ export default async function handler(req, res) {
     const data = JSON.parse(req.body);
 
     if (data.temppass == process.env.TEMPORARY_PASSWORD) {
-      res.json({ message: await prisma.Todo.create({ data: { name: data.name } }) });
+      return res.json({ message: await prisma.Todo.create({ data: { name: data.name } }) });
     }
-    res.status(500).json({ message: 'Authentication failed' });
+    return res.status(500).json({ message: 'Authentication failed' });
   } catch (error) {
     return res.status(500).json({ message: 'There was an unknown error' });
   }
